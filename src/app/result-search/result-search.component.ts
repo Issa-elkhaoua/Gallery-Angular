2ce4507c05e36ec0a13c94904e8871590366accd
Issa-elkhaoua <issa.elkhaoua2001@gmail.com>
2024-01-19 22:53:48 +0100

    search result

--- src/app/result-search/result-search.component.ts
+++ src/app/result-search/result-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedDataService } from '../shared-data.service';
+import { Lightbox } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-result-search',
@@ -9,12 +10,49 @@ import { SharedDataService } from '../shared-data.service';
 export class ResultSearchComponent implements OnInit{
 
   resultArray!: string[];
+  _album: any[] = [];
+  isLoading = true; 
 
-  constructor(private sharedDataService: SharedDataService) {}
+  constructor(private sharedDataService: SharedDataService,
+    private lightbox: Lightbox,
+    ) {
+    this.resultArray = this.sharedDataService.sharedResultArray;
+    for (let imagePath of this.resultArray) {
+      const src = `assets/tb/${imagePath}.jpg`;
+      const caption = 'Image caption here';
+      const thumb = imagePath;
+      const album = {
+         src: src,
+         caption: caption,
+         thumb: thumb
+      };
+
+      this._album.push(album);
+    }
+
+  }
 
   ngOnInit(): void {
-    // Accède au tableau partagé depuis le service
     this.resultArray = this.sharedDataService.sharedResultArray;
+  }
+
+
+  imageLoaded(): void {
+    this.isLoading = false;
+  }
+
+  open(index: number): void {
+    this.lightbox.open(this._album, index);
+    const fullPath = this._album[index].src;
+    // const trimmedPath = this.trimImagePath(fullPath);
+    // console.log('Trimmed Image path:', trimmedPath);
+
+    // const objFilePath = `assets/3d/${trimmedPath}.obj`;
+
+    // console.log('objFilePath path:', objFilePath);
+
+
+
   }
 
 }
